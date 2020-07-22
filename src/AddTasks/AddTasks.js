2cf499b312338ff7df385a1c8d5d7cc44906c2e6
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-07-22 21:10:47 +0600

    Added function for adding tasks

--- src/AddTasks/AddTasks.js
+++ src/AddTasks/AddTasks.js
@@ -5,8 +5,8 @@ const AddTasks = (props) => {
     return (
         <div className="add-tasks-box">
             <form className="add-tasks__form">
-                <input type="text" className="add-tasks__text" placeholder="Enter your task here..."/>
-                <button type="submit" className="add-tasks__btn">Add Task</button>
+                <input type="text" className="add-tasks__text" value={props.newTask} onChange={props.onAddNewTask} placeholder="Enter your task here..."/>
+                <button type="submit" className="add-tasks__btn" onClick={props.onAddNewTaskInList}>Add Task</button>
             </form>
         </div>
     );
--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {nanoid} from "nanoid";
 import './App.css';
+import Task from "./Tasks/Task";
+import AddTasks from "./AddTasks/AddTasks";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState({
+      tasksList: [
+          {task: 'Buy milk', id: nanoid(6)},
+          {task: 'Do homework', id: nanoid(6)},
+          {task: 'Read book', id: nanoid(6)},
+      ],
+      newTask: '',
+  });
+
+  const onAddNewTask = event => {
+      let newTask = event.target.value;
+      setTasks(state => ({...state, newTask}));
+  }
+
+  const onAddNewTaskInList = event => {
+      event.preventDefault();
+      const tasksList = [...tasks.tasksList];
+      const newTask = tasks.newTask;
+      tasksList.push({task: newTask, id: nanoid(6)});
+      setTasks({tasksList, newTask: ""});
+  }
+
   return (
     <div className="App">
-
+      <div className="container">
+          <h1 className="title">ToDo List</h1>
+          <AddTasks newTask={tasks.newTask} onAddNewTask={event => onAddNewTask(event)} onAddNewTaskInList={event => onAddNewTaskInList(event)}/>
+        {tasks.tasksList.map((item) => <Task key={item.id} text={item.task}/>)}
+      </div>
     </div>
   );
 }
