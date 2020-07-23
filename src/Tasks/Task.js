0acd9a699754c311540355bf19c2370085521130
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-07-23 16:42:00 +0600

    Added done tasks props

--- src/Tasks/Task.js
+++ src/Tasks/Task.js
@@ -3,8 +3,9 @@ import "./Task.css";
 
 const Task = (props) => {
     return (
-        <div className='task'>
-            <p className='task__text'>{props.text}</p>
+        <div className={props.className}>
+            <input type="checkbox" className="done-checkbox" name="done-task" id={props.id} checked={props.done} onChange={props.doneTask}/>
+            <label className='task__text' htmlFor={props.id}>{props.text}</label>
             <button className='task__btn-close' onClick={props.removeTask}>&times;</button>
         </div>
     );
