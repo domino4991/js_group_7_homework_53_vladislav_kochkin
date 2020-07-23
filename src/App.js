6d8c4f4d16b690077a99774529d08d6cd31230a1
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-07-23 16:40:43 +0600

    Added doneTask function

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import AddTasks from "./AddTasks/AddTasks";
 const App = () => {
   const [tasks, setTasks] = useState({
       tasksList: [
-          {task: 'Buy milk', id: nanoid(6)},
-          {task: 'Do homework', id: nanoid(6)},
-          {task: 'Read book', id: nanoid(6)},
+          {task: 'Buy milk', id: nanoid(6), done: false, className: 'task'},
+          {task: 'Do homework', id: nanoid(6), done: false, className: 'task'},
+          {task: 'Read book', id: nanoid(6), done: false, className: 'task'},
       ],
       newTask: '',
   });
@@ -24,7 +24,7 @@ const App = () => {
       if(tasks.newTask) {
           const tasksList = [...tasks.tasksList];
           const newTask = tasks.newTask;
-          tasksList.push({task: newTask, id: nanoid(6)});
+          tasksList.push({task: newTask, id: nanoid(6), done: false, className: 'task'});
           setTasks({tasksList, newTask: ''});
       }
   }
@@ -36,6 +36,18 @@ const App = () => {
       setTasks({tasksList, newTask: ''});
   }
 
+  const doneTask = id => {
+      const index = tasks.tasksList.findIndex(d => d.id === id);
+      const tasksList = [...tasks.tasksList];
+      tasksList[index].done = !tasksList[index].done;
+      if(tasksList[index].done) {
+          tasksList[index].className += ' done-task';
+      } else {
+          tasksList[index].className = 'task';
+      }
+      setTasks(state => ({tasksList, ...state}));
+  }
+
   return (
     <div className="App">
       <div className="container">
@@ -45,7 +57,14 @@ const App = () => {
               onAddNewTask={event => addNewTask(event)}
               onAddNewTaskInList={event => addNewTaskInList(event)}
           />
-        {tasks.tasksList.map((item) => <Task key={item.id} text={item.task} removeTask={() => removeTask(item.id)}/>)}
+        {tasks.tasksList.map((item) => <Task
+            key={item.id}
+            id={item.id}
+            className={item.className}
+            doneTask={() => doneTask(item.id)}
+            text={item.task}
+            removeTask={() => removeTask(item.id)}
+        />)}
       </div>
     </div>
   );
